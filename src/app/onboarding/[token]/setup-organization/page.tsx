2f5472811b7cee831bcb2c183073199d163becb6
Mahima-Sanketh-Git <mahimadevops@gmail.com>
2026-03-20 13:04:12 +0530

    up v2

--- src/app/onboarding/[token]/setup-organization/page.tsx
+++ src/app/onboarding/[token]/setup-organization/page.tsx
@@ -122,7 +122,7 @@ const Page = () => {
     };
 
     try {
-      const response = await axios.put('https://api.cynoguard.com/api/onboarding/sync',
+      const response = await axios.put('http://127.0.0.1:4000/api/onboarding/sync',
        payload,
        {
         headers:{
